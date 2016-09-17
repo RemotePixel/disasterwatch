fdf76d4fba7a4624c0d4d306be3d9768783f3038
vincentsarago <vincent.sarago@gmail.com>
2016-09-16 22:29:26 -0400

    split app.js & style & event update

---
+++ js/images_download.js
@@ -0,0 +1,164 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*global mapboxgl, mapboxgl, alert*/
+/*global moment, moment, alert*/
+/*global console, console, alert*/
+
+function feeddownloadS2(elem, preview) {
+    "use strict";
+    var s2prefix = "http://sentinel-s2-l1c.s3.amazonaws.com/";
+    $('#modalDownloadS2 .overview').attr('data-id', elem);
+    $('#modalDownloadS2 .overview').attr('data-prev', preview);
+    $('#modalDownloadS2 .dwn-bands').append(
+        '<span>Direct Download S2 band (Right Click on link)</span>' +
+            '<a id="b1" target="_blank" href="' + s2prefix + elem + '/B01.jp2" download>B1 - Coastal (60m)</a>' +
+            '<a id="b2" target="_blank" href="' + s2prefix + elem + '/B02.jp2" download>B2 - Blue (10m)</a>' +
+            '<a id="b3" target="_blank" href="' + s2prefix + elem + '/B03.jp2" download>B3 - Green (10m)</a>' +
+            '<a id="b4" target="_blank" href="' + s2prefix + elem + '/B04.jp2" download>B4 - Red (10m)</a>' +
+            '<a id="b5" target="_blank" href="' + s2prefix + elem + '/B05.jp2" download>B5 - Vegetation Classif 1 (20m)</a>' +
+            '<a id="b6" target="_blank" href="' + s2prefix + elem + '/B06.jp2" download>B6 - Vegetation Classif 2 (20m)</a>' +
+            '<a id="b7" target="_blank" href="' + s2prefix + elem + '/B07.jp2" download>B7 - Vegetation Classif 3 (20m)</a>' +
+            '<a id="b8" target="_blank" href="' + s2prefix + elem + '/B08.jp2" download>B8 - Near Infrared (10m)</a>' +
+            '<a id="b9" target="_blank" href="' + s2prefix + elem + '/B09.jp2" download>B9 - Water vapour (60m)</a>' +
+            '<a id="b10" target="_blank" href="' + s2prefix + elem + '/B10.jp2" download>B10 - Cirrus (60m)</a>' +
+            '<a id="b11" target="_blank" href="' + s2prefix + elem + '/B11.jp2" download>B11 - Thermal Infrared 1 (20m)</a>' +
+            '<a id="b12" target="_blank" href="' + s2prefix + elem + '/B12.jp2" download>B12 - Thermal Infrared 2 (20m)</a>' +
+            '<a id="mtl" target="_blank" href="' + s2prefix + elem + '/productInfo.json" download>Metadata</a>'
+    );
+    $('#modalDownloadS2 .overview').html('<img src="' + preview + '">');
+    $('#modalDownloadS2').modal();
+}
+
+function feeddownloadL8(url, id) {
+    "use strict";
+    $('#modalDownloadL8 .overview').attr('data-id', id);
+    $('#modalDownloadL8 .dwn-bands').append(
+        '<span>Direct Download L8 band (Right Click on link)</span>' +
+            '<a id="b1" target="_blank" href="' + url + id + '_B1.TIF" download>B1 - Coastal aerosol</a>' +
+            '<a id="b2" target="_blank" href="' + url + id + '_B2.TIF" download>B2 - Blue</a>' +
+            '<a id="b3" target="_blank" href="' + url + id + '_B3.TIF" download>B3 - Green</a>' +
+            '<a id="b4" target="_blank" href="' + url + id + '_B4.TIF" download>B4 - Red</a>' +
+            '<a id="b5" target="_blank" href="' + url + id + '_B5.TIF" download>B5 - Near Infrared</a>' +
+            '<a id="b6" target="_blank" href="' + url + id + '_B6.TIF" download>B6 - Shortwave Infrared 1</a>' +
+            '<a id="b7" target="_blank" href="' + url + id + '_B7.TIF" download>B7 - Shortwave Infrared 2</a>' +
+            '<a id="b8" target="_blank" href="' + url + id + '_B8.TIF" download>B8 - Panchromatic (15m)</a>' +
+            '<a id="b9" target="_blank" href="' + url + id + '_B9.TIF" download>B9 - Cirrus</a>' +
+            '<a id="b10" target="_blank" href="' + url + id + '_B10.TIF" download>B10 - Thermal Infrared 1</a>' +
+            '<a id="b11" target="_blank" href="' + url + id + '_B11.TIF" download>B11 - Thermal Infrared 2</a>' +
+            '<a id="bQA" target="_blank" href="' + url + id + '_BQA.TIF" download>BQA - Quality Assessment</a>' +
+            '<a id="mtl" target="_blank" href="' + url + id + '_MTL.txt" download>MTL - Metadata</a>'
+    );
+
+    var req = {
+        scene: id,
+        bands: "[4,3,2]"
+    };
+
+    $.post("https://npj2g2bwcc.execute-api.us-west-2.amazonaws.com/landsat/overview", JSON.stringify({info: req}))
+        .done(function (data) {
+            if (!(data.hasOwnProperty('errorMessage'))) {
+                $('#modalDownloadL8 .overview').html('<img src="data:image/png;base64,' + data.data + '">');
+            } else {
+                $('#modalDownloadL8 .overview').html('<span>Preview Unavailable</span>');
+            }
+        })
+        .fail(function () {
+            $('#modalDownloadL8 .overview').html('<span>Preview Unavailable</span>');
+        });
+    $('#modalDownloadL8').modal();
+}
+
+$('#modalDownloadL8').on('hidden.bs.modal', function () {
+    "use strict";
+    $("#modalPreview").focus();
+    $('#modalDownloadL8 .dwn-bands').empty();
+    $('#modalDownloadL8 .overview').attr('data-id', '');
+    $('#modalDownloadL8 .overview').html('<span><i class="fa fa-spinner fa-spin"></i></span>');
+
+    $("#modalDownloadL8 .dropdown-menu li a").each(function () {
+        $(this).removeClass('on');
+    });
+    $("#modalDownloadL8 .dropdown-menu li a").first().addClass("on");
+    $("#modalDownloadL8 .dropdown .btn:first-child").html($("#modalDownloadL8 .dropdown-menu li a").first().text() + ' <span class="caret"></span>');
+});
+
+$('#modalDownloadS2').on('hidden.bs.modal', function () {
+    "use strict";
+    $("#modalPreview").focus();
+    $('#modalDownloadS2 .dwn-bands').empty();
+    $('#modalDownloadS2 .overview').attr('data-id', '');
+    $('#modalDownloadS2 .overview').html('<span><i class="fa fa-spinner fa-spin"></i></span>');
+
+    $("#modalDownloadS2 .dropdown-menu li a").each(function () {
+        $(this).removeClass('on');
+    });
+    $("#modalDownloadS2 .dropdown-menu li a").first().addClass("on");
+    $("#modalDownloadS2 .dropdown .btn:first-child").html($("#modalDownloadL8 .dropdown-menu li a").first().text() + ' <span class="caret"></span>');
+});
+
+$(function () {
+    "use strict";
+
+    $("#modalDownloadS2 .dropdown-menu li a").click(function () {
+        $('#modalDownloadS2 .overview').html('<span><i class="fa fa-spinner fa-spin"></i></span>');
+        $("#modalDownloadS2 .dropdown .btn:first-child").html($(this).text() + ' <span class="caret"></span>');
+
+        var req = {
+            path: $('#modalDownloadS2 .overview').attr("data-id"),
+            bands: $(this).parent().attr("data-bands")
+        };
+
+        if (req.bands === "[4,3,2]") {
+            var preview = $('#modalDownloadS2 .overview').attr("data-prev");
+            $('#modalDownloadS2 .overview').html('<img src="' + preview + '">');
+        } else {
+            $.post("https://6bu43holc0.execute-api.eu-central-1.amazonaws.com/prod/sentinel2_ovr", JSON.stringify({info: req}))
+                .done(function (data) {
+                    if (!(data.hasOwnProperty('errorMessage'))) {
+                        $('#modalDownloadS2 .overview').html('<img src="data:image/png;base64,' + data.data + '">');
+                    } else {
+                        $('#modalDownloadS2 .overview').html('<span>Preview Unavailable</span>');
+                    }
+                })
+                .fail(function () {
+                    $('#modalDownloadS2 .overview').html('<span>Preview Unavailable</span>');
+                });
+        }
+
+        $("#modalDownloadS2 .dropdown-menu li a").each(function () {
+            $(this).removeClass('on');
+        });
+        $(this).addClass('on');
+    });
+});
+
+$(function () {
+    "use strict";
+
+    $("#modalDownloadL8 .dropdown-menu li a").click(function () {
+        $('#modalDownloadL8 .overview').html('<span><i class="fa fa-spinner fa-spin"></i></span>');
+        $("#modalDownloadL8 .dropdown .btn:first-child").html($(this).text() + ' <span class="caret"></span>');
+
+        var req = {
+            scene: $('#modalDownloadL8 .overview').attr("data-id"),
+            bands: $(this).parent().attr("data-bands")
+        };
+
+        $.post("https://npj2g2bwcc.execute-api.us-west-2.amazonaws.com/landsat/overview", JSON.stringify({info: req}))
+            .done(function (data) {
+                if (!(data.hasOwnProperty('errorMessage'))) {
+                    $('#modalDownloadL8 .overview').html('<img src="data:image/png;base64,' + data.data + '">');
+                } else {
+                    $('#modalDownloadL8 .overview').html('<span>Preview Unavailable</span>');
+                }
+            })
+            .fail(function () {
+                $('#modalDownloadL8 .overview').html('<span>Preview Unavailable</span>');
+            });
+
+        $("#modalDownloadL8 .dropdown-menu li a").each(function () {
+            $(this).removeClass('on');
+        });
+        $(this).addClass('on');
+    });
+});
---
+++ js/map.js
@@ -0,0 +1,426 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*global mapboxgl, mapboxgl, alert*/
+/*global moment, moment, alert*/
+/*global turf, turf, alert*/
+/*global console, console, alert*/
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidmluY2VudHNhcmFnbyIsImEiOiJjaWlleG1vdmowMWhydGtrc2xqcmQzNmhlIn0.80HAFLCQ6yUWCk4mwm6zbw';
+var map = new mapboxgl.Map({
+        container: 'map',
+        center: [0, 10],
+        zoom: 1,
+        style: 'mapbox://styles/mapbox/light-v9',
+        attributionControl: true,
+        minZoom: 0,
+        maxZoom: 8
+    }),
+    draw = mapboxgl.Draw({
+        displayControlsDefault: false
+    });
+
+map.dragRotate.disable();
+map.touchZoomRotate.disableRotation();
+
+map.addControl(draw);
+map.addControl(new mapboxgl.Navigation());
+
+map.on('style.load', function () {
+    "use strict";
+    //Landsat 8 Layer
+    map.addSource('landsat', {
+        "type": "vector",
+        "url": "mapbox://vincentsarago.8ib6ynrs"
+    });
+    map.addLayer({
+        "id": "l8-highlighted",
+        "type": "fill",
+        "source": "landsat",
+        "source-layer": "Landsat8_Desc_filtr2",
+        "paint": {
+            "fill-outline-color": "#1386af",
+            "fill-color": "#0f6d8e",
+            "fill-opacity": 0.3
+        },
+        "filter": ["in", "PATH", ""]
+    });
+
+    //Sentinel 2 Layer
+    map.addSource('sentinel2', {
+        "type": "vector",
+        "url": "mapbox://vincentsarago.0qowxm38"
+    });
+    map.addLayer({
+        "id": "s2-highlighted",
+        "type": "fill",
+        "source": "sentinel2",
+        "source-layer": "Sentinel2_Grid",
+        "paint": {
+            "fill-outline-color": "#1386af",
+            "fill-color": "#0f6d8e",
+            "fill-opacity": 0.3
+        },
+        "filter": ["in", "Name", ""]
+    });
+
+    // DisasterDB
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+
+    map.addSource('disasterdb', {
+        'type': 'geojson',
+        'data': geojson
+    });
+
+    map.addLayer({
+        "id": "disasterdb-points",
+        "type": "circle",
+        "source": "disasterdb",
+        "filter": ["==", "$type", "Point"],
+        "paint": {
+            "circle-color": '#da7979',
+            'circle-radius': {
+                "base": 1.8,
+                'stops': [
+                    [0, 2],
+                    [9, 10]
+                ]
+            }
+        }
+    });
+
+    map.addLayer({
+        "id": "disasterdb-polygons",
+        "type": "fill",
+        "source": "disasterdb",
+        "filter": ["==", "$type", "Polygon"],
+        "paint": {
+            "fill-outline-color": "#da7979",
+            "fill-color": "#ba3e3e",
+            "fill-opacity": 0.4
+        }
+    });
+
+    // USGS Latest EarthQuakes
+    map.addSource('earthquakes', {
+        'type': 'geojson',
+        'data': geojson
+    });
+
+    map.addLayer({
+        "id": "earthquakes-blur",
+        "type": "circle",
+        "source": "earthquakes",
+        "maxzoom": 9,
+        "layout": {'visibility' : 'none'},
+        "paint": {
+            "circle-opacity": 0.8,
+            'circle-radius': {
+                'property': 'mag',
+                "base": 1.8,
+                'stops': [
+                    [{zoom: 0,  value: 2}, 0.25],
+                    [{zoom: 0,  value: 8}, 16],
+                    [{zoom: 9, value: 2}, 10],
+                    [{zoom: 9, value: 8}, 100]
+                ]
+            },
+            'circle-color': {
+                property: 'mag',
+                stops: [
+                    ['4.5', '#fff'],
+                    ['8', '#f00']
+                ]
+            }
+        }
+    });
+
+    map.addLayer({
+        "id": "earthquakes-point",
+        "type": "circle",
+        "source": "earthquakes",
+        "layout": {'visibility' : 'none'},
+        "paint": {
+            "circle-color": 'rgb(23, 14, 3)',
+            'circle-radius': {
+                "base": 1.1,
+                "stops": [
+                    [0, 0.5],
+                    [8, 3]
+                ]
+            }
+        }
+    });
+
+    // EONET Events
+    map.addSource('eonet', {
+        'type': 'geojson',
+        'data': geojson
+    });
+
+    map.addLayer({
+        "id": "eonet-point",
+        "type": "symbol",
+        "source": "eonet",
+        "layout": {
+            "visibility" : "none",
+            "icon-image": "{icon}-15"
+        }
+    });
+
+    map.on('mousemove', function (e) {
+        var mouseRadius = 1,
+            feature = map.queryRenderedFeatures([
+                [e.point.x - mouseRadius, e.point.y - mouseRadius],
+                [e.point.x + mouseRadius, e.point.y + mouseRadius]
+            ], {layers: ["eonet-point", "earthquakes-point", "disasterdb-points", "disasterdb-polygons"]})[0];
+
+        if (feature) {
+            map.getCanvas().style.cursor = 'pointer';
+
+        } else {
+            map.getCanvas().style.cursor = 'inherit';
+        }
+
+    }).on('click', function (e) {
+        var mouseRadius = 1;
+        if (map.getLayer("earthquakes-point").getLayoutProperty('visibility') !== 'none') {
+            var feature = map.queryRenderedFeatures([
+                [e.point.x - mouseRadius, e.point.y - mouseRadius],
+                [e.point.x + mouseRadius, e.point.y + mouseRadius]
+            ], {layers: ["earthquakes-point"]})[0];
+
+            if (feature) {
+                var popup = new mapboxgl.Popup()
+                    .setLngLat(e.lngLat)
+                    .setHTML('<div class="linetab bold">Name: ' + feature.properties.title + '</div>' +
+                                '<div class="linetab">Date: ' + moment(feature.properties.time).utc().format('YYYY-MM-DD HH:mm:ss') + '(UTC)</div>' +
+                                '<div class="linetab">Magnitude: ' + feature.properties.mag + '</div>' +
+                                '<div class="linetab">Felt: ' + ((feature.properties.felt === null) ? 'No' : 'Yes') + '</div>' +
+                                '<div class="linetab">Duration (min): ' + feature.properties.dmin + '</div>' +
+                                '<div class="linetab">Tsunami: ' + ((feature.properties.tsunami === 0) ? 'No' : 'Yes') + '</div>' +
+                                '<div class="linetab"><a target="_blank" href="' + feature.properties.url + '">Info</a></div>' +
+                                '<div class="linetab"><a class="link" onclick="seeEQimages(\'' + feature.properties.detail + '\')">See Images</a></div>')
+                    .addTo(map);
+            }
+        }
+
+        if (map.getLayer("eonet-point").getLayoutProperty('visibility') !== 'none') {
+            var feature = map.queryRenderedFeatures([
+                [e.point.x - mouseRadius, e.point.y - mouseRadius],
+                [e.point.x + mouseRadius, e.point.y + mouseRadius]
+            ], {layers: ["eonet-point"]})[0];
+
+            if (feature) {
+                var links = '',
+                    sources = JSON.parse(feature.properties.sources);
+
+                for (var j = 0; j < sources.length; j++) {
+                    links += '<a target="_blank" href="' + sources[j].url + '">' + sources[j].id + '</a> '
+                }
+
+                var popup = new mapboxgl.Popup()
+                    .setLngLat(e.lngLat)
+                    .setHTML('<div class="linetab bold">Name: ' + feature.properties.title + '</div>' +
+                                '<div class="linetab">Date: ' + moment(feature.properties.date).format('YYYY-MM-DD HH:mm:ss') + '(UTC)</div>' +
+                                '<div class="linetab">Type: ' + feature.properties.dtype + '</div>' +
+                                '<div class="linetab">Description: ' + feature.properties.description + '</div>' +
+                                '<div class="linetab">Links: ' + links + '</div>' +
+                                '<div class="linetab"><a class="link" onclick="seeEONETimages(\'' + feature.properties.id + '\')">See Images</a></div>')
+                    .addTo(map);
+            }
+        }
+
+        var feature = map.queryRenderedFeatures([
+            [e.point.x - mouseRadius, e.point.y - mouseRadius],
+            [e.point.x + mouseRadius, e.point.y + mouseRadius]
+        ], {layers: ["disasterdb-points", "disasterdb-polygons"]})[0];
+
+        if (feature) {
+            var dtype = '',
+                sources = JSON.parse(feature.properties.dtype);
+
+            for (var j = 0; j < sources.length; j++) {
+                dtype += '<span class="' + sources[j] + '">' + sources[j] + '</span> '
+            }
+
+            var popup = new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML('<div class="linetab bold">Name: ' + feature.properties.name + '</div>' +
+                            '<div class="linetab disasterType">Type: ' + dtype + '</div>' +
+                            '<div class="linetab">Location: ' + feature.properties.place + '</div>' +
+                            '<div class="linetab">Start Date: ' + feature.properties.dateStart + '</div>' +
+                            '<div class="linetab">End Date: ' + feature.properties.dateEnd + '</div>' +
+                            '<div class="linetab">Comments: <br>' + feature.properties.comments + '</div>' +
+                            '<div class="linetab"><a onclick="seeEvtDBimages(\'' + feature.properties.uuid + '\')">See Images</a></div>' +
+                            '<div class="linetab">' +
+                                // '<a onclick="subscribeEvt(\'' + feature.properties.uuid + '\')">Subscribe</a> | ' +
+                                '<a onclick="editEvt(\'' + feature.properties.uuid + '\')">Update</a> | ' +
+                                '<a onclick="removeEvt(\'' + feature.properties.uuid + '\')">Remove</a>' +
+                            '</div>')
+                .addTo(map);
+        }
+    });
+
+    var slider = document.getElementById('slider'),
+        sliderValue = document.getElementById('slider-value');
+
+    slider.addEventListener('input', function (e) {
+        if (map.getSource("gibs-tiles")) {
+            map.setPaintProperty('gibs-tiles', 'raster-opacity', parseInt(e.target.value, 10) / 100);
+        }
+        sliderValue.textContent = e.target.value + '%';
+    });
+
+    //Wrapping in d3-queue - loading app ?
+    getDisasterdb();
+    getEarthquake();
+    getEONETEvents();
+
+});
+
+////////////////////////////////////////////////////////////////////////////////
+//Check if User
+map.on('draw.selectionchange', function (e) {
+    "use strict";
+    if (!$(".leftblock").hasClass('in')) {
+        if (e.features.length !== 0) {
+            $("#modalQuestion").modal();
+        }
+    }
+});
+
+map.on('draw.create', function (e) {
+    "use strict";
+
+    // limit draw Polygons size ??
+
+    openleftBlock();
+    getImages();
+
+    if (e.features[0].geometry.type === "Polygon") {
+        var bbox = turf.extent(e.features[0].geometry);
+        map.fitBounds(bbox, {padding: 20});
+    }
+
+    if (e.features[0].geometry.type === "Point") {
+        var round = turf.buffer(e.features[0], 100, 'kilometers'),
+            bbox = turf.extent(round);
+        map.fitBounds(bbox, {padding: 20});
+    }
+});
+
+
+function setStyle(basename) {
+    "use strict";
+
+    $(".date-button").attr('disabled', 'disabled');
+    $("#slider").attr('disabled', 'disabled');
+
+    if (map.getSource("gibs-tiles")) {
+        map.removeLayer("gibs-tiles");
+        map.removeSource("gibs-tiles");
+    }
+
+    switch (basename) {
+    case 'MapboxMap':
+        return;
+    default:
+        $(".date-button").attr('disabled', false);
+        $("#slider").attr('disabled', false);
+        var dateValue = document.getElementsByClassName('date-button')[0].textContent,
+            basemaps_url = "https://map1.vis.earthdata.nasa.gov/wmts-webmerc/" + basename + "/default/" + dateValue + "/GoogleMapsCompatible_Level9/{z}/{y}/{x}.jpg";
+
+        map.addSource('gibs-tiles', {
+            'type': 'raster',
+            'tiles': [
+                basemaps_url
+            ],
+            'attribution' : [
+                '<a href="https://earthdata.nasa.gov/about/science-system-description/eosdis-components/global-imagery-browse-services-gibs" >NASA EOSDIS GIBS</a>'
+            ],
+            'tileSize': 256
+        });
+
+        var slider = document.getElementById('slider'),
+            opa = (parseInt(slider.getAttribute('value'), 10) / 100);
+
+        map.addLayer({
+            'id': 'gibs-tiles',
+            'type': 'raster',
+            'source': 'gibs-tiles',
+            "minzoom": 1,
+            "maxzoom": 9,
+            'paint': {"raster-opacity": opa}
+        }, "admin-2-boundaries-bg");
+    }
+}
+
+function changeOverlay(overlay) {
+    "use strict";
+    $("#basemaps-panel .side-view-content .side-element .link-on").each(function () {
+        $(this).removeClass('on');
+    });
+
+    $("#" + overlay + " .link-on").addClass('on');
+    setStyle(overlay);
+}
+
+function update_basemaps() {
+    "use strict";
+    var overlay = document.getElementsByClassName('link-on on')[0].parentElement.getAttribute('id');
+    setStyle(overlay);
+}
+
+function drawOnMap(type) {
+    "use strict";
+
+    draw.deleteAll();
+
+    switch (type) {
+    case 'point':
+        draw.changeMode('draw_point');
+        break;
+
+    case 'polygon':
+        draw.changeMode('draw_polygon');
+        break;
+
+    case null:
+        break;
+    }
+}
+
+function hoverS2(gr) {
+    "use strict";
+    map.setFilter("s2-highlighted", gr);
+}
+
+function hoverL8(gr) {
+    "use strict";
+    map.setFilter("l8-highlighted", gr);
+}
+
+$("#earthquake-checkbox").change(function () {
+    "use strict";
+    $("#earthquake-checkbox").parent().toggleClass('green');
+    if (document.getElementById("earthquake-checkbox").checked) {
+        map.setLayoutProperty('earthquakes-point', 'visibility', 'visible');
+        map.setLayoutProperty('earthquakes-blur', 'visibility', 'visible');
+    } else {
+        map.setLayoutProperty('earthquakes-point', 'visibility', 'none');
+        map.setLayoutProperty('earthquakes-blur', 'visibility', 'none');
+    }
+});
+
+$("#eonet-checkbox").change(function () {
+    "use strict";
+    $("#eonet-checkbox").parent().toggleClass('green');
+    if (document.getElementById("eonet-checkbox").checked) {
+        map.setLayoutProperty('eonet-point', 'visibility', 'visible');
+    } else {
+        map.setLayoutProperty('eonet-point', 'visibility', 'none');
+    }
+});
