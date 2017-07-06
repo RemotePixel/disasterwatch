7cd86e44e60ed2478f35fb21657949fe1b6e1ad2
vincentsarago <vincent.sarago@mapbox.com>
2017-07-05 23:30:53 -0400

    update rpix api

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const rpix_api_us = 'https://kbbaqpm7th.execute-api.us-west-2.amazonaws.com/production/';
-const rpix_api_eu = 'https://l14fuypvcl.execute-api.eu-central-1.amazonaws.com/production/';
+const rpix_api_us = 'https://lhb7x0r2bf.execute-api.us-west-2.amazonaws.com/production/';
+const rpix_api_eu = 'https://lm0w96zbz4.execute-api.eu-central-1.amazonaws.com/production/';
 
 const volcanoes_api_url ='https://joqhmu5n9g.execute-api.us-east-1.amazonaws.com/production';
 const disasterwatch_api_url = 'https://jriian4se3.execute-api.us-east-1.amazonaws.com/production';
