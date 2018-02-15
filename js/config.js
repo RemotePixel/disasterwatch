731a005a05b39e01969bd497951908da29733b1f
vincentsarago <vincent.sarago@mapbox.com>
2018-02-15 11:47:57 -0500

    fixed untracked files

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const rpix_api_us = '{RPIX-US-URL}'; //https://github.com/RemotePixel/remotepixel-api
-const rpix_api_eu = '{RPIX-EU-URL}'; //https://github.com/RemotePixel/remotepixel-api
-const landsat_tiler_url = '{LANDSAT-TILER}'; //https://github.com/mapbox/landsat-tiler
-const sentinel_tiler_url = '{SENTINEL-TILER}'; //https://github.com/mapbox/sentinel-tiler
-const volcanoes_api_url ='https://joqhmu5n9g.execute-api.us-east-1.amazonaws.com/production';
-const disasterwatch_api_url = '{DW-API}'; //https://github.com/RemotePixel/disasterwatch-lambda
-const sat_api_url = 'https://api.developmentseed.org/satellites/';
-const MAPBOX_ACCESS_TOKEN = '{YOUR-TOKEN}';
